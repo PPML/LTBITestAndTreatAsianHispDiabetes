991029c33a19e101342f0f2662d823b90e7ce305
nswartwo <nswartwo@gmail.com>
2024-11-13 00:40:34 -0500

    Adds the MITUS package to be loaded

--- code/setupEnvironAndDeps.R
+++ code/setupEnvironAndDeps.R
@@ -1,10 +1,10 @@
 ###############################################################################
-##### Load in libraries 
+##### Load in libraries
 ###############################################################################
-library(MITUS) 
+library(MITUS)
 library(survey)
 library(foreign)
-library(plyr) 
+library(plyr)
 library(MASS)
 library(stringr)
 library(ggplot2)
@@ -15,16 +15,16 @@ library(ipumsr)
 library(purrr)
 
 ###############################################################################
-##### Define working directory 
+##### Define working directory
 ###############################################################################
 setwd("~/TTTAsianHispDiabetes/LTBITestAndTreatAsianHispDiabetes/")
 
 ###############################################################################
-##### Define palette 
+##### Define palette
 ###############################################################################
-safe_colorblind_palette <- c("#88CCEE", "#CC6677", "#DDCC77", 
-                             "#117733", "#332288", "#AA4499", 
-                             "#44AA99", "#999933", "#882255", 
+safe_colorblind_palette <- c("#88CCEE", "#CC6677", "#DDCC77",
+                             "#117733", "#332288", "#AA4499",
+                             "#44AA99", "#999933", "#882255",
                              "#661100", "#6699CC", "#888888")
 
 ###############################################################################
@@ -47,7 +47,7 @@ betapar <- function(tgt) {
     tgt <- as.numeric(tgt)
     mn <- tgt[1]; cir <- (tgt[3]-tgt[2])
     xopt <- function(xx,mn=mn,cir=cir) {
-        cir2 <- qbeta(c(1,39)/40,xx*(mn/(1-mn)),xx); 
+        cir2 <- qbeta(c(1,39)/40,xx*(mn/(1-mn)),xx);
         cir2 <- cir2[2]-cir2[1]
         sum((cir2-cir)^2) }
     zz <- optimize(xopt,c(0.2,100000),mn=mn,cir=cir)$minimum
